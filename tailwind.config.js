ffed4b9ec3fe87684b694489b143eb8d3fcfaa66
cmramos <cmramos@bancolombia.com.co>
2024-10-06 15:21:02 -0500

    Init project and basic styles

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', 'system-ui', 'sans-serif']
+    },
+    extend: {
+      spacing: {
+        'header-height': '4rem'
+      },
+      colors: {
+        'bg-body': '#F0F2F5',
+        'title-color': '#404145',
+        'text-color': '#62646A',
+      },
+      backgroundImage: {
+        'hero-home': "url('/src/assets/hero-home.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
