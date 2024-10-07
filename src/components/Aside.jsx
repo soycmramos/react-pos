d693753387a7d3cd1c555cd20ccbde91bfdcc8eb
cmramos <cmramos@bancolombia.com.co>
2024-10-06 21:55:45 -0500

    Set sticky flow

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -2,7 +2,7 @@ import Nav from './Nav'
 
 export default function Aside() {
 	return (
-		<aside className='bg-white border-r border-gray-300'>
+		<aside className='fixed top-header-height left-0 w-1/5 h-full bg-white border-r border-gray-300'>
 			<Nav />
 		</aside>
 	)
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import Container from './Container'
 
 export default function Footer() {
 	return (
-		<footer className='bg-neutral-900 py-4 px-10 text-center text-gray-100'>
+		<footer className='sticky bg-neutral-900 py-4 px-10 text-center text-gray-100'>
 			<Container>
 				<p>{new Date().getFullYear()}</p>
 			</Container>
