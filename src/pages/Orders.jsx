a3c498dd15a54aaa1b6b86e914fd0ec870320c86
cmramos <cmramos@bancolombia.com.co>
2024-10-06 21:18:50 -0500

    Add icons in order table

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,3 +1,5 @@
+import { FiUser, FiCalendar } from 'react-icons/fi'
+
 export default function Orders() {
 	document.title = 'Orders'
 
@@ -22,30 +24,40 @@ export default function Orders() {
 				<table className='bg-white p-6 w-full text-left'>
 					<thead>
 						<tr>
-							<th className='py-3 border-gray-300 font-semibold'>Cliente</th>
-							<th className='py-3 border-gray-300 font-semibold'>Fecha</th>
-							<th className='py-3 border-gray-300 font-semibold text-center'>Total</th>
-							<th className='py-3 border-gray-300 font-semibold text-center'>Estado</th>
-							<th className='py-3 border-gray-300 font-semibold'></th>
+							<th className='p-4 border-gray-300 font-semibold'>Cliente</th>
+							<th className='p-4 border-gray-300 font-semibold'>Fecha</th>
+							<th className='p-4 border-gray-300 font-semibold text-center'>Total</th>
+							<th className='p-4 border-gray-300 font-semibold text-center'>Estado</th>
+							<th className='p-4 border-gray-300 font-semibold'></th>
 						</tr>
 					</thead>
 					<tbody>
 						{
-							arr.map(x => {
+							arr.map((x, i) => {
+								i++
 								const color = x === 0 ? 'green' : 'red'
 								const content = x === 0 ? 'Cancelado' : 'Sin cancelar'
 								return (
 									<tr className='odd:bg-gray-100' key={Math.random()}>
-										<td className='py-3 border-y border-gray-300 flex items-center'>Carlos Mario Ramos Pérez</td>
-										<td className='py-3 border-y border-gray-300'>{date}</td>
-										<td className='py-3 border-y border-gray-300 text-center'>$ 5.000</td>
-										<td className='py-3 border-y border-gray-300 text-center'>
-											<span
-												className={`font-semibold py-1 px-2 text-sm bg-${color}-500/10 text-${color}-600 border border-${color}-200 rounded`}>
+										<td className='p-4 border-y border-gray-300'>
+											<FiUser className='mr-1 mb-1 w-6 h-auto inline-block' />
+											<span>
+												Carlos Mario Ramos Pérez
+											</span>
+										</td>
+										<td className='p-4 border-y border-gray-300'>
+											<FiCalendar className='mr-1 mb-1 w-6 h-auto inline-block' />
+											<span>
+												{date}
+											</span>
+										</td>
+										<td className='p-4 border-y border-gray-300 text-center'>$ {i * 3500}</td>
+										<td className='p-4 border-y border-gray-300 text-center'>
+											<span className={`font-semibold py-1 px-2 text-sm bg-${color}-500/10 text-${color}-600 border border-${color}-200 rounded`}>
 												{content}
 											</span>
 										</td>
-										<td className='py-3 border-y border-gray-300 text-center'>Editar</td>
+										<td className='p-4 border-y border-gray-300 text-center'>Editar</td>
 									</tr>
 								)
 							})
