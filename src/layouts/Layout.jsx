c7e871e65d38ad398018ae01789a73c684c5d141
soycmramos <soycmramos@gmail.com>
2025-03-25 15:22:19 -0500

    Fix Layout component

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,10 +1,10 @@
 import Header from './../components/Header'
 
-const Layout = ({ Children }) => {
+const Layout = ({ children }) => {
 	return (
 		<>
 			<Header />
-			{Children}
+			{children}
 		</>
 	)
 }
