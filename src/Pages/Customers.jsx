1953b08002fd1785ae6cc38889cf6516b28621c2
soycmramos <soycmramos@gmail.com>
2025-03-25 21:20:44 -0500

    Add customers fallback

--- src/Pages/Customers.jsx
+++ src/Pages/Customers.jsx
@@ -1,14 +1,60 @@
+import { useState, useEffect } from 'react'
 import { twMerge } from 'tailwind-merge'
 import { clsx } from 'clsx'
 import Container from '../components/Container'
 import Button from './../components/Button'
 import Input from './../components/Input'
+import Loader from '../components/Loader'
 import { FaRegUser, FaEllipsis, FaPlus, } from 'react-icons/fa6'
-import { useState } from 'react'
+import noDataImg from '../assets/no-data.svg'
 
 const Customers = () => {
 
 	const [isActive, setIsActive] = useState(true)
+	const [customers, setCustomers] = useState([])
+	const [search, setSearch] = useState('')
+	const [isLoading, setIsLoading] = useState(true)
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const response = await fetch('http://localhost:5000/customers')
+				const { data } = await response.json()
+				setCustomers(data)
+			} catch (error) {
+				console.error(error)
+				return
+			} finally {
+				setIsLoading(false)
+			}
+		})()
+	}, [])
+
+	const handleChange = e => {
+		setSearch(e.target.value)
+		const result = customers.filter(customer => customer.name.toLowerCase().includes(search.toLowerCase()))
+		setCustomers(result)
+	}
+
+	if (isLoading) {
+		return (
+			<Loader />
+		)
+	}
+
+	const NoCustomers = () => {
+		return (
+			<div className='flex flex-col gap-4 py-12'>
+				<img src={noDataImg} alt='No data' className='max-w-1/6 mx-auto' />
+				<p className='text-center'>No hay clientes por le momento. Comienza por crear uno.</p>
+				<div className='flex justify-center'>
+					<Button variant='primary' className='inline-flex items-center gap-1'>
+						<FaPlus />Crear nuevo cliente
+					</Button>
+				</div>
+			</div>
+		)
+	}
 
 	return (
 		<section className='py-8'>
@@ -17,41 +63,62 @@ const Customers = () => {
 					<h1 className='text-4xl font-semibold'>Clientes</h1>
 				</div>
 				<div className='flex justify-end gap-2 mb-4'>
-					<Input type='search' name='filter-customer' placeholder='Buscar...' className='inline-flex w-auto' />
+					{
+						customers.length > 0 && (
+							<Input
+								type='search'
+								name='filter-customer'
+								placeholder='Buscar...'
+								className='inline-flex w-auto'
+								value={search}
+								onChange={handleChange}
+							/>
+						)
+					}
 					<Button variant='primary-outlined' className='inline-flex items-center gap-1'>
 						<FaPlus />Crear nuevo cliente
 					</Button>
 				</div>
-				<div className='p-4 bg-white shadow rounded'>
-					<table className='table-fixed w-full mx-auto'>
-						<thead>
-							<tr className='border-b border-gray-300 text-gray-500'>
-								<th className='p-3 w-1/4 font-normal text-left'>Nombre</th>
-								<th className='p-3 w-1/4 font-normal text-center'>Estado</th>
-								<th className='p-3 w-1/4 font-normal'>Acciones</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr className='border-b border-gray-300 hover:bg-gray-100 transition-colors'>
-								<td className='p-3 flex items-center gap-1'>
-									<FaRegUser className='size-7' />
-									Carlos Mario Ramos Pérez
-								</td>
-								<td className='p-3 text-center'>
-									<span className={twMerge(clsx('py-1 px-2 text-sm font-semibold rounded', {
-										'bg-success/25 text-green-900': isActive,
-										'bg-danger/25 text-red-900': !isActive,
-									}))}>
-										{(isActive && 'Activo') || (!isActive && 'Inactivo')}
-									</span>
-								</td>
-								<td className='p-3'>
-									<FaEllipsis className='size-7 cursor-pointer mx-auto' />
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
+				{
+					!customers.length > 0
+						? <NoCustomers />
+						: (
+							<div className='p-4 bg-white shadow rounded'>
+								<table className='table-fixed w-full mx-auto'>
+									<thead>
+										<tr className='border-b border-gray-300 text-gray-500'>
+											<th className='p-3 w-1/4 font-normal text-left'>Nombre</th>
+											<th className='p-3 w-1/4 font-normal text-center'>Estado</th>
+											<th className='p-3 w-1/4 font-normal'>Acciones</th>
+										</tr>
+									</thead>
+									<tbody>
+										{customers.map(customer => {
+											return (
+												<tr className='border-b border-gray-300 hover:bg-gray-100 transition-colors' key={customer.id}>
+													<td className='p-3 flex items-center gap-1'>
+														<FaRegUser className='size-7' />
+														{customer.name}
+													</td>
+													<td className='p-3 text-center'>
+														<span className={twMerge(clsx('py-1 px-2 text-sm font-semibold rounded', {
+															'bg-success/25 text-green-900': isActive,
+															'bg-danger/25 text-red-900': !isActive,
+														}))}>
+															{(isActive && 'Activo') || (!isActive && 'Inactivo')}
+														</span>
+													</td>
+													<td className='p-3'>
+														<FaEllipsis className='size-7 cursor-pointer mx-auto' />
+													</td>
+												</tr>
+											)
+										})}
+									</tbody>
+								</table>
+							</div>
+						)
+				}
 			</Container>
 		</section>
 	)
---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+export const Loader = () => {
+	return (
+		<div className='h-screen grid place-items-center'>
+			<span className='block rounded-full border-12 border-primary/50 border-t-primary size-28 animate-spin'></span>
+		</div>
+	)
+}
+
+export default Loader
