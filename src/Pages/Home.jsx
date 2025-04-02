164b04ddb2dfe5000188c9c7a6e85ab34741108b
soycmramos <soycmramos@gmail.com>
2025-04-02 16:24:54 -0500

    Delete provisional Nav

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,8 @@
-import { Link } from 'react-router-dom'
 import Container from './../components/Container'
 
 const Home = () => {
 	return (
 		<Container>
-			<section className='py-8'>
-				<nav>
-					<ul className='flex gap-x-4'>
-						<li><Link to='/customers' className='hover:text-text-color/75'>Clientes</Link></li>
-						<li><Link to='/products' className='hover:text-text-color/75'>Productos</Link></li>
-					</ul>
-				</nav>
-			</section>
 		</Container>
 	)
 }
