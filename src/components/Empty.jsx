06c53d63ad53ec397073940466cd9a85863f75fe
soycmramos <soycmramos@gmail.com>
2025-04-01 19:28:40 -0500

    Fix vertical padding

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -2,7 +2,7 @@ import empty from '../assets/empty.svg'
 
 const Empty = ({ message, children }) => {
 	return (
-		<div className='flex flex-col gap-y-4'>
+		<div className='flex flex-col gap-y-4 p-12'>
 			<img src={empty} alt='No data' className='max-w-1/6 mx-auto' />
 			<p className='text-center'>{message}</p>
 			<div className='flex justify-center'>
