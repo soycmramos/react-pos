604ed08c53cc816558b6d6e15a53152bd0816eb2
soycmramos <soycmramos@gmail.com>
2025-04-02 16:24:21 -0500

    Refactor Loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 export const Loader = () => {
 	return (
-		<div className='h-screen absolute inset-0 grid place-items-center'>
-			<span className='block size-28 rounded-full border-12 border-primary/50 border-t-primary animate-spin'></span>
+		<div className='h-full grid place-content-center'>
+			<span className='block size-28 rounded-full border-12 border-primary/25 border-t-primary animate-spin'></span>
 		</div>
 	)
 }
